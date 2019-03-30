cc851ecab328cd641c683a4c077317dea48b0107
Muhammad Ghazallah <ghazallah64@gmail.com>
2019-03-30 18:45:27 +0200

    updating service layer structure

--- EcommeceApplication/src/main/java/controller/brandServlets/CreateBrand.java
+++ EcommeceApplication/src/main/java/controller/brandServlets/CreateBrand.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package services.userServices;
+package controller.brandServlets;
 
 /**
  *
  * @author ghazallah
  */
-public class RegisterService {
+public class CreateBrand {
     
 }
--- EcommeceApplication/src/main/java/services/CategoryServices.java
+++ EcommeceApplication/src/main/java/services/CategoryServices.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package services.userServices;
+package services;
 
 /**
  *
  * @author ghazallah
  */
-public class LoginService {
+public class CategoryServices {
     
 }
