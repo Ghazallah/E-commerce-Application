489c490073e2b03823ae7100077ff6eafde2cd77
Muhammad Ghazallah <ghazallah64@gmail.com>
2019-04-01 04:08:19 +0200

    adding login filter logic

--- EcommeceApplication/src/main/java/filters/LoginFilter.java
+++ EcommeceApplication/src/main/java/filters/LoginFilter.java
@@ -5,15 +5,46 @@
  */
 package filters;
 
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author ghazallah
- * 
  */
-public class LoginFilter {
+public class LoginFilter implements Filter{
+
     
+
+    @Override
+    public void doFilter(ServletRequest sRequest, ServletResponse sResponse, FilterChain fc) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) sRequest;
+        HttpServletResponse response = (HttpServletResponse)sResponse;
+        HttpSession session = request.getSession(false);
+        if (session ==null){
+            //response.sendRedirect("login.html");
+            //refirect here on login page
+        }
+        
+        fc.doFilter(sRequest, sResponse);
+    }
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
     
-    //every request must pass through this filter to determine
-    // if is logged in or not
     
 }
